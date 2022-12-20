5fa4eeb2d5bfcbaccf4560e421302ad516ceec63
olbukovskiy <olbukovskiy@gmail.com>
2022-12-20 12:26:46 +0200

    hw 5

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ export const theme = {
     heading: 'inherit',
     monospace: 'Menlo, monospace',
   },
-  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96, 192],
   fontWeights: {
     body: 400,
     heading: 700,
